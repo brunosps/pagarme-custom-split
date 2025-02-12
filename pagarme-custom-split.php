be52d574b0f055f26b0c48244a9a8366b0dbbea9
Bruno Santos <bruno@neometa.com.br>
2025-02-12 15:18:20 -0300

    Alterado plugin

--- pagarme-custom-split.php
+++ pagarme-custom-split.php
@@ -14,49 +14,57 @@ if (!defined('ABSPATH')) {
 if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) return;
 if (!in_array('woocommerce-pagarme/woocommerce-pagarme.php', apply_filters('active_plugins', get_option('active_plugins')))) return;
 
-// Registra o autoloader personalizado
-spl_autoload_register('custom_pagarme_order_autoload', true, true);
+class Pagarme_Custom_Split_Plugin {
+    public function __construct() {
+        // Registra o autoloader personalizado
+        spl_autoload_register(array($this, 'custom_pagarme_order_autoload'), true, true);
+    }
 
-function custom_pagarme_order_autoload($class) {
-    if ($class === 'Pagarme\Core\Payment\Aggregates\Order') {
-        class Custom_Pagarme_Order_Wrapper {
-            private $originalOrder;
+    public function custom_pagarme_order_autoload($class) {
+        if ($class === 'Pagarme\Core\Payment\Aggregates\Order') {
+            class Custom_Pagarme_Order_Wrapper {
+                private $originalOrder;
 
-            public function __construct() {
-                $this->originalOrder = new \Pagarme\Core\Payment\Aggregates\Order();
-            }
-
-            public function __call($method, $arguments) {
-                if ($method === 'convertToSDKRequest') {
-                    return $this->customConvertToSDKRequest();
+                public function __construct() {
+                    $this->originalOrder = new \Pagarme\Core\Payment\Aggregates\Order();
                 }
-                return call_user_func_array([$this->originalOrder, $method], $arguments);
-            }
 
-            private function customConvertToSDKRequest() {
-                $orderRequest = $this->originalOrder->convertToSDKRequest();
-
-                if (!empty($this->originalOrder->getSplitData())) {
-                    $splitData = $this->originalOrder->getSplitData();
-                    
-                    global $wp_filter;
-                    if (isset($wp_filter['do_split_order'])) {
-                        $orderRequest = apply_filters('do_split_order', $orderRequest, $splitData, $this->originalOrder);
+                public function __call($method, $arguments) {
+                    if ($method === 'convertToSDKRequest') {
+                        return $this->customConvertToSDKRequest();
                     }
+                    return call_user_func_array([$this->originalOrder, $method], $arguments);
                 }
 
-                return $orderRequest;
+                private function customConvertToSDKRequest() {
+                    $orderRequest = $this->originalOrder->convertToSDKRequest();
+
+                    if (!empty($this->originalOrder->getSplitData())) {
+                        $splitData = $this->originalOrder->getSplitData();
+                        
+                        global $wp_filter;
+                        if (isset($wp_filter['do_split_order'])) {
+                            $orderRequest = apply_filters('do_split_order', $orderRequest, $splitData, $this->originalOrder);
+                        }
+                    }
+
+                    return $orderRequest;
+                }
+
+                public function __get($name) {
+                    return $this->originalOrder->$name;
+                }
+
+                public function __set($name, $value) {
+                    $this->originalOrder->$name = $value;
+                }
             }
 
-            public function __get($name) {
-                return $this->originalOrder->$name;
-            }
-
-            public function __set($name, $value) {
-                $this->originalOrder->$name = $value;
-            }
+            class_alias('Custom_Pagarme_Order_Wrapper', $class);
         }
-
-        class_alias('Custom_Pagarme_Order_Wrapper', $class);
     }
 }
+
+// Initialize the plugin
+new Pagarme_Custom_Split_Plugin();
+
